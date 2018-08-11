7642daa314769765109f456c841b6f19743075ee
league <league@wts10.attlocal.net>
2018-08-10 17:00:42 -0700

    Did more of whack a mole

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,8 +1,12 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class WhackAMole
+public class WhackAMole implements ActionListener
 {
 	
 	public static void main(String args[])
@@ -21,27 +25,29 @@ public class WhackAMole
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
 		
+		Random r = new Random();
+		int random = r.nextInt(9) + 1;
+		drawButtons(random);
 	}
 	
 	void drawButtons(int num)
 	{
-		JButton button1 = new JButton();
-		panel.add(button1);
-		JButton button2 = new JButton();
-		panel.add(button2);
-		JButton button3 = new JButton();
-		panel.add(button3);
-		JButton button4 = new JButton();
-		panel.add(button4);
-		JButton button5 = new JButton();
-		panel.add(button5);
-		JButton button6 = new JButton();
-		panel.add(button6);
-		JButton button7 = new JButton();
-		panel.add(button7);
-		JButton button8 = new JButton();
-		panel.add(button8);
-		JButton button9 = new JButton();
-		panel.add(button9);
+		for (int i = 0; i < 9; i++)
+		{
+			JButton button = new JButton();
+			panel.add(button);
+			button.addActionListener(this);
+			if(i == num)
+			{
+				button.setText("MOLE");
+			}
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 }
