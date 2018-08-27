88651dff2ca60749173babc1b320be6d40919b89
league <league@wts-imac-11.attlocal.net>
2018-08-26 17:31:07 -0700

    Worked more on whack a mole

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,9 +1,11 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener
@@ -18,6 +20,11 @@ public class WhackAMole implements ActionListener
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	
+	Date timeAtStart = new Date();
+	
+	int whacks = 0;
+	int score = 0;
+	
 	void createGUI()
 	{
 		frame.setVisible(true);
@@ -25,13 +32,20 @@ public class WhackAMole implements ActionListener
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
 		
+		drawButtons(createRandom());
+	}
+	
+	int createRandom()
+	{
 		Random r = new Random();
-		int random = r.nextInt(9) + 1;
-		drawButtons(random);
+		int random = r.nextInt(9);
+		return random;
 	}
 	
 	void drawButtons(int num)
 	{
+		panel.removeAll();
+		
 		for (int i = 0; i < 9; i++)
 		{
 			JButton button = new JButton();
@@ -43,11 +57,45 @@ public class WhackAMole implements ActionListener
 			}
 		}
 	}
-
+	
+	void speak(String words) {
+	     try {
+	          Runtime.getRuntime().exec("say " + words).waitFor();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	
+	private void endGame(Date timeAtStart, int molesWhacked) {
+	     Date timeAtEnd = new Date();
+	     JOptionPane.showMessageDialog(null, "Your whack rate is "
+	          + (molesWhacked / ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00))
+	          + " moles per second.");
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton)e.getSource();
+		whacks++;
 		
+			if(buttonPressed.getText() == "MOLE")
+			{
+				score++;
+			}
+			else
+			{
+				speak("miss");
+			}
+			
+			if(score >= 10)
+			{
+				endGame(timeAtStart, score);
+			}
+			else
+			{
+				frame.dispose();
+				createGUI();
+			}
 	}
 }
